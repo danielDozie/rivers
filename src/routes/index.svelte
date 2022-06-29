c7f4f2d11e469e689a8249652ae66fed6b138878
badt0men <placmedia@gmail.com>
2022-06-30 00:18:43 +0100

    ...

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,7 +2,7 @@
     import Header from "$lib/sections/Header.svelte";
     import Hero from '$lib/sections/Hero.svelte';
     import About from '$lib/sections/About.svelte'
-    //import Events from '$lib/sections/Events.svelte'
+    import Events from '$lib/sections/Events.svelte'
     import ChatRoom from '$lib/sections/ChatRoom.svelte'
     import Publications from "../lib/sections/Publications.svelte";
     import Footer from '$lib/sections/Footer.svelte'
@@ -12,7 +12,7 @@
 <Header />
 <Hero />
 <About />
-<!-- <Events /> -->
+<Events />
 <ChatRoom />
 <Publications />
 <Footer />
