205615bf35e5ce55c4f44736ae418ceb45320d5a
badt0men <placmedia@gmail.com>
2022-06-30 02:56:48 +0100

    .......

--- src/lib/sections/Header.svelte
+++ src/lib/sections/Header.svelte
@@ -2,6 +2,8 @@
 	import RegisterButton from '$lib/components/Button/RegisterButton.svelte';
 	import Menu from '$lib/components/Menu/Menu.svelte';
 	let siteTitle = 'SustainableRIVERS';
+	import {mobileMenuStore} from '$lib/store.svelte'
+	import {toggleMenu} from '$lib/components/Others/MenuToggle.svelte';
 </script>
 
 <svelte:head>
@@ -21,6 +23,17 @@
 	<!-- Mobile Menu -->
 	<div class="flex md:hidden mx-auto pt-4 justify-between px-4">
 		<a href="/"><img src="/logo.svg" alt="" class="drop-shadow-sm w-8 mt-2" /></a>
-		<img src="/menuIcon.svg" alt="" class="drop-shadow-sm h-8 mt-2 cursor-pointer" />
+		<img src="/menuIcon.svg" alt="" class="drop-shadow-sm h-8 mt-2 cursor-pointer" on:click={toggleMenu} />
 	</div>
+	<!-- Off canvas mobile menu -->
+	<div class={`${$mobileMenuStore ? 'hidden' : 'flex'} w-full h-screen md:hidden bg-black/30 mx-auto mt-6 relative`}>
+		<div class="w-3/5 h-full bg-white pt-4">
+			<div class="flex flex-col mx-auto px-4">
+				<h1 class="text-2xl text-black/80">MENU</h1>
+				<Menu />
+				<RegisterButton />
+			</div>
+		</div>
+	</div>
+	<!-- Off canvas mobile menu end -->
 </div>
