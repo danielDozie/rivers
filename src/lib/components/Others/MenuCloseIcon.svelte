79b1d9fcbdc8c62f8eabe0e6cfbd8f9a96af3bbb
badt0men <placmedia@gmail.com>
2022-06-30 19:01:57 +0100

    .......

---
+++ src/lib/components/Others/MenuCloseIcon.svelte
@@ -0,0 +1,5 @@
+<script>
+    import {toggleMenu} from '$lib/components/Others/Toggle.svelte';
+</script>
+
+<img src="/menuIcon.svg" alt="" class="drop-shadow-sm h-8 mt-2 cursor-pointer" on:click={toggleMenu} />
