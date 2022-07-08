5a1dcdbfd3aaa197b01e4ff7c5063ac9a7300061
badt0men <placmedia@gmail.com>
2022-07-08 03:41:56 +0100

    ......

--- src/lib/database.js
+++ src/lib/database.js
@@ -4,8 +4,8 @@ dotenv.config()
 
 import { MongoClient } from 'mongodb'
 
-//const uri = `mongodb://${process.env.DBUSER}:${process.env.DBPASS}@localhost`
-const uri = `mongodb://localhost`
+const uri = `mongodb://${process.env.DBUSER}:${process.env.DBPASS}@localhost`
+//const uri = `mongodb://localhost`
 const options = {} //if you need to set the user & pass optionally.
   
 const client = new MongoClient(uri, options)
